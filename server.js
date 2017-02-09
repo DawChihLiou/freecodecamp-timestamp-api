1a4d00b542d712f5b5ac51117dffa24501ebd0a0
Dawchih Liou <dawochih.liou@gmail.com>
2017-02-09 09:16:27 +0000

    unix/natural route

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const app = express();
 
+// index page
 app.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+app.get('/:date', (req, res) => {
+    'use strict';
+    
+    const timestampRegex = /[0-9]{13}|[0-9]{10}/;
+    const options        = { year: 'numeric', month: 'long', day: 'numeric' };
+    const param          = req.params.date;
+    
+    let unix    = null; 
+    let natural = null;
+    let date;
+    
+    if (timestampRegex.test(parseInt(param))) {
+        let ts = parseInt(param);
+        
+        date    = new Date(ts.length === 10 ? ts * 1000 : ts);
+        unix    = ts;
+        natural = date.toLocaleDateString('en-US', options);
+    } else if (param === (new Date(param)).toLocaleDateString('en-US', options)) {
+        date    = new Date(param);
+        unix    = date.getTime() / 1000;
+        natural = param;
+    }
+    
+    res.send({ unix, natural });
 });
 
 app.listen(8080, () => {
